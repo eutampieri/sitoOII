8844da84f11bb2df4151828223c0615cd0445b8b
eutampieri <eugenio@etcloud.ddns.net>
2017-09-27 12:19:57 +0200

    .

--- register.php
+++ register.php
@@ -1,2 +1,6 @@
 <?php
-$qry="CREATE TABLE Utenti (Username TEXT, Password TEXT)"
+/*
+https://docs.google.com/spreadsheets/d/1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA/export?format=csv&id=1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA
+*/
+
+$qry="CREATE TABLE Utenti (Username TEXT, Password TEXT, )";
