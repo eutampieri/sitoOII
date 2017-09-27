c55422283f3fd8b4362640b6f15e3ea6958e04d2
eutampieri <eugenio@etcloud.ddns.net>
2017-09-27 13:14:44 +0200

    Definita struttura DB

--- register.php
+++ register.php
@@ -1,10 +1,4 @@
 <?php
-/*
-https://docs.google.com/spreadsheets/d/1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA/export?format=csv&id=1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA
-*/
-
-$query = "CREATE TABLE Utenti (Username TEXT, Password TEXT, )";
-
 function getUserData($phone)
 {
     $csv = file_get_contents("https://docs.google.com/spreadsheets/d/1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA/export?format=csv&id=1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA");
@@ -21,3 +15,4 @@ function getUserData($phone)
         if($data[3] == $phone) return ["cognome" => data[0], "nome" => data[1], "classe" => data[2]];
     }
 }
+$query = "CREATE TABLE Utenti (Username TEXT, Password TEXT, )";
