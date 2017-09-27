04deb568512b2abcf07f518c30e28514ff144a9a
eutampieri <eugenio@etcloud.ddns.net>
2017-09-27 11:46:59 +0200

    Aggiunta API per ottenere l'URL del PDF del problema

--- res/js/problemaCMS.js
+++ res/js/problemaCMS.js
@@ -1,7 +1,19 @@
-function geetTaskUrl(task){
-    /*tsk={"action":"get","name":task}
-    s=requests.post("https://cms.di.unipi.it/api/task",data=json.dumps(tsk),headers={"Content-Type":"application/json"})
-    tsk=json.loads(s.text)
-    st=tsk["statements"]["it"]
-    url="https://cms.di.unipi.it/api/files/"+st+"/"+task+".pdf" */   
+function getUrlPromise(url, func) {
+    return new Promise(function(resolve,reject){
+    var webrequest = new XMLHttpRequest();
+    webrequest.open('GET', url, true);
+    webrequest.onload=function(){
+        resolve(webrequest.responseText);
+    };
+    webrequest.send(null);});
+}
+function getTaskUrl(task){
+    return new Promise(function(resolve,reject){
+        var url
+        getUrlPromise(urlBN()+"res/cmsTaskApi.php?task="+encodeURIComponent(task)).then(function(tskJson){
+            tskDta=JSON.parse(tskJson);
+            url="https://cms.di.unipi.it/api/files/"+tskDta.statements.it+"/"+task+".pdf";
+        });
+        resolve(url);
+    });
 }
