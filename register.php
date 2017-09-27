3510334b6dcdf09392d90c8303c5312f4d382ec2
Rebecca Monti <Rebecca@MacBook-Pro-di-Rebecca.local>
2017-09-27 12:33:44 +0200

    Scritta funzione per ottenere dati da Drive.

--- register.php
+++ register.php
@@ -3,4 +3,21 @@
 https://docs.google.com/spreadsheets/d/1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA/export?format=csv&id=1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA
 */
 
-$qry="CREATE TABLE Utenti (Username TEXT, Password TEXT, )";
+$query = "CREATE TABLE Utenti (Username TEXT, Password TEXT, )";
+
+function getUserData($phone)
+{
+    $csv = file_get_contents("https://docs.google.com/spreadsheets/d/1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA/export?format=csv&id=1Iv8y74oteOicW8DCUi7BEHrMfldZZTHeZy0GmOao2aA");
+    $rows = explode("\n", $csv);
+    $index = false;
+    foreach($rows as $row)
+    {
+        if(!index)
+        {
+            $index = true;
+            continue;
+        }
+        $data = explode(",", $row);
+        if($data[3] == $phone) return ["cognome" => data[0], "nome" => data[1], "classe" => data[2]];
+    }
+}
