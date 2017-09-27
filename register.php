0365f39ce7e04aff2ad3d9e312211c0bd7fc155d
eutampieri <eugenio@etcloud.ddns.net>
2017-09-27 13:43:38 +0200

    Implementata la registrazione :party:

--- register.php
+++ register.php
@@ -20,6 +20,7 @@ CREATE TABLE Utenti (Username TEXT, Password TEXT, CMSUser TEXT, CMSPwd TEXT, Em
 CREATE TABLE Posts(Titolo TEXT, Contenuto TEXT, Data INTEGER, Autore TEXT);
 CREATE TABLE Risorse(Nome TEXT, File BLOB, Autore TEXT, Data INTEGER)";
 CREATE TABLE Notifiche(Username TEXT, JSON TEXT);
+CREATE TABLE Sessioni (ID TEXT, Username TEXT);
 EOF;
 
 $createDB = false;
@@ -32,5 +33,23 @@ if(createDB)
     $stmt = $database->prepare($query); // statement
     $stmt->execute();
 }
-
+$datiUtente=getUserData($_POST["telefono"]);
+$qry="INSERT INTO Utenti VALUES(:user , :password , :userCMS , :passwordCMS , :email , :telefono , :nome , :cognome , :classe)";
+$stmt=$database->prepare($qry);
+$stmt->bindParam(':username', $_POST["username"]);
+$stmt->bindParam(':password', $_POST["password"]);
+$stmt->bindParam(':userCMS', $_POST["userCMS"]);
+$stmt->bindParam(':passwordCMS', $_POST["passwordCMS"]);
+$stmt->bindParam(':email', $_POST["email"]);
+$stmt->bindParam(':telefono', $_POST["telefono"]);
+$stmt->bindParam(':nome', $datiUtente["nome"]);
+$stmt->bindParam(':cognome', $datiUtente["cognome"]);
+$stmt->bindParam(':classe', $datiUtente["classe"]);
+$stmt->execute();
+$sessID=uniqid("sess");
+setcookie("sessione",$sessID);
+$qry="INSERT INTO Sessioni VALUES(:id , :user)";
+$stmt->bindParam(':id', $sessID);
+$stmt->bindParam(':user', $_POST["username"]);
+header("Location: index.html");
 // $stmt->bindParam(parameter, variable);
