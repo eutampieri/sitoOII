482aea6e528bb1f537adbac85cd45b5fcf489a31
eutampieri <eugenio@etcloud.ddns.net>
2017-09-27 10:54:30 +0200

    Inizio testo cms

---
+++ res/js/problemaCMS.js
@@ -0,0 +1,7 @@
+function geetTaskUrl(task){
+    /*tsk={"action":"get","name":task}
+    s=requests.post("https://cms.di.unipi.it/api/task",data=json.dumps(tsk),headers={"Content-Type":"application/json"})
+    tsk=json.loads(s.text)
+    st=tsk["statements"]["it"]
+    url="https://cms.di.unipi.it/api/files/"+st+"/"+task+".pdf" */   
+}
