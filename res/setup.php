216f607437fdb069d27ad814e0c6df0cf2fbb7bb
eutampieri <eugenio@etcloud.ddns.net>
2017-11-03 16:14:52 +0100

    .

--- res/setup.php
+++ res/setup.php
@@ -1,37 +1,32 @@
 <?php
 $query = <<<EOF
-CREATE TABLE Utenti (Username TEXT, Password TEXT, CMSUser TEXT, Email TEXT, Nome TEXT, Cognome TEXT, Classe TEXT)
-CREATE TABLE Post(Titolo TEXT, Contenuto TEXT, Data INTEGER, Autore TEXT)
-CREATE TABLE Risorse(Nome TEXT, File BLOB, Autore TEXT, Data INTEGER)
-CREATE TABLE Notifiche(Username TEXT, JSON TEXT)
-CREATE TABLE Sessioni (ID TEXT, Username TEXT)
-CREATE TABLE Tutor (CMSUser TEXT)
-CREATE TABLE APIKeys (Servizio TEXT, Chiave TEXT)
-CREATE TABLE RifClassifica (CMSUser TEXT)
-CREATE TABLE Eventi (Descrizione TEXT, Inizio INTEGER, Fine INTEGER, Tipo TEXT);
+CREATE TABLE IF NOT EXISTS Utenti (Username TEXT, Password TEXT, CMSUser TEXT, Email TEXT, Nome TEXT, Cognome TEXT, Classe TEXT)
+CREATE TABLE IF NOT EXISTS Post(Titolo TEXT, Contenuto TEXT, Data INTEGER, Autore TEXT)
+CREATE TABLE IF NOT EXISTS Risorse(Nome TEXT, File BLOB, Autore TEXT, Data INTEGER)
+CREATE TABLE IF NOT EXISTS Notifiche(Username TEXT, JSON TEXT)
+CREATE TABLE IF NOT EXISTS Sessioni (ID TEXT, Username TEXT)
+CREATE TABLE IF NOT EXISTS Tutor (CMSUser TEXT, Bio TEXT, Image BLOB)
+CREATE TABLE IF NOT EXISTS APIKeys (Servizio TEXT, Chiave TEXT)
+CREATE TABLE IF NOT EXISTS RifClassifica (CMSUser TEXT)
+CREATE TABLE IF NOT EXISTS Eventi (Descrizione TEXT, Inizio INTEGER, Fine INTEGER, Tipo TEXT);
+CREATE TABLE IF NOT EXISTS CodeTryOut (Problema TEXT, Sorgente TEXT, Autore TEXT, Problemi INTEGER);
 EOF;
 $qry1=<<<EOF
-CREATE TABLE "Bugs" ("ID" TEXT PRIMARY KEY  NOT NULL ,"affects" TEXT,"notify" TEXT,"code" TEXT,"language" TEXT,"creator" TEXT,"fixed" BOOL,"date" DATETIME DEFAULT (null) ,"comment" TEXT,"desc" TEXT,"tags" TEXT)
-CREATE TABLE "Fixes" ("ID" TEXT PRIMARY KEY  NOT NULL  UNIQUE , "BugID" TEXT, "fixer" TEXT, "date" DATETIME, "votes" INTEGER, "code" TEXT, "comment" TEXT)
+CREATE TABLE IF NOT EXISTS "Bugs" ("ID" TEXT PRIMARY KEY  NOT NULL ,"affects" TEXT,"notify" TEXT,"code" TEXT,"language" TEXT,"creator" TEXT,"fixed" BOOL,"date" DATETIME DEFAULT (null) ,"comment" TEXT,"desc" TEXT,"tags" TEXT)
+CREATE TABLE IF NOT EXISTS "Fixes" ("ID" TEXT PRIMARY KEY  NOT NULL  UNIQUE , "BugID" TEXT, "fixer" TEXT, "date" DATETIME, "votes" INTEGER, "code" TEXT, "comment" TEXT)
 EOF;
 if(isset($_POST["action"])){
-    $createDB = false;
-    if(!is_file("db.sqlite")) $createDB = true;
     $database = new PDO("sqlite:db.sqlite");
     $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $database1 = new PDO("sqlite:bugs.sqlite");
     $database1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    if($createDB)
-    {
-        foreach(explode("\n",$query) as $q){
-            $stmt = $database->prepare($q);
-            $stmt->execute();
-        }
-        foreach(explode("\n",$qry1) as $q){
-            $stmt = $database1->prepare($q);
-            $stmt->execute();
-        }
+    foreach(explode("\n",$query) as $q){
+        $stmt = $database->prepare($q);
+        $stmt->execute();
+    }
+    foreach(explode("\n",$qry1) as $q){
+        $stmt = $database1->prepare($q);
+        $stmt->execute();
     }
     $listaTutor=json_decode($_POST["tutors"],true);
     $qry="INSERT INTO Tutor VALUES (:u);";
